d69ca09ef8b205bad02f05e4ee3488a624e25827
Yntxfr <yntxfr71@pm.me>
2024-06-01 11:48:25 +0200

    primer commit

---
+++ ExamenProyecto1/src/main/java/com/proyecto1/ExamenProyecto1/ExamenProyecto1Application.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyecto1.ExamenProyecto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
+
+/**
+ *
+ * @author: Alex Fernandez
+ */
+
+@SpringBootApplication
+public class ExamenProyecto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenProyecto1Application.class, args);
+	}
+
+}
---
+++ ExamenProyecto1/src/test/java/com/proyecto1/ExamenProyecto1/ExamenProyecto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto1.ExamenProyecto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenProyecto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
